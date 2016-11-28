420e809b938346650d0ef00be2fb8439c4180791
tkachuk <dev.trauret@gmail.com>
2016-11-28 19:04:19 +0300

    Add buttons to show document and custon data links list

--- app/js/test.js
+++ app/js/test.js
@@ -1,50 +1,86 @@
 (function() {
-    var renderedColorBlocks = [];
+    $(function() {
+        window.app = new app();
+    });
 
-    function addColorBlocks() {
-        var target = $('.wrap');
-        if (!target[0]) return;
+    function app() {
+        this.html = '';
+        this.target = $('.wrap');
+        this.rendered = {
+            block: [],
+            part: []
+        };
+
+        this.loadDocumentJSON(this.target);
+        $('#html').hide();
+
+        this.renderDocument = function() {
+            $('#html').show();
+        };
+
+        this.showLinks = function() {
+            var links = '';
+
+            $.each(this.matches, function(index, match) {
+                var blockIndex = parseInt(match.replace('%%', ''));
+                links += '<li><a href="#block_' + blockIndex + '" onclick="app.gotoColorBlock(' + blockIndex + ')">' + blockIndex + '</a></li>';
+            });
+
+            $('#links').html(links).show();
+        };
+
+        this.gotoColorPart = function(blockIndex) {
+            this.rendered.part[blockIndex] = this.renderColorBlock(this.target, blockIndex, 'part');
+        };
+
+        this.gotoColorBlock = function(blockIndex) {
+            $('#html').show();
+            this.rendered.block[blockIndex] = this.renderColorBlock(this.target, blockIndex, 'block');
+        };
+    }
+
+    app.prototype.addColorBlocks = function() {
+        if (!this.target[0]) return;
 
         var enter = /%%(\d+)\s/g;
         var close = /\s(\d+)%%/g;
 
-        var matches = target[0].innerHTML.match(enter);
+        var matches = this.target[0].innerHTML.match(enter);
         if (!matches) return;
 
-        var result = target[0].innerHTML.replace(enter, '<div id="block_$1_enter" class="block_$1 block_start"><a name="block_$1"></a></div>');
-        result = result.replace(close, '<div id="block_$1_close" class="block_$1 block_close"></div>');
+        var result = this.target[0].innerHTML.replace(enter, '<span id="block_$1_enter" class="block_$1 block_start"><a name="block_$1"></a></span>');
+        result = result.replace(close, '<span id="block_$1_close" class="block_$1 block_close"></span>');
 
-        target[0].innerHTML = result;
-    }
+        this.target[0].innerHTML = result;
 
-    function renderColorBlock(blockIndex) {
-        var target = $('.wrap');
-        if (renderedColorBlocks[blockIndex]) return;
+        return this.matches = matches;
+    };
+
+    app.prototype.renderColorBlock = function(target, blockIndex, type) {
+        if (this.rendered[type][blockIndex]) return;
+        if (!type) type = 'block';
 
         var targetWidth = target.width();
-        var startBlock = $('#block_' + blockIndex + '_enter');
-        var closeBlock = $('#block_' + blockIndex + '_close');
+        var startBlock = $('#' + type + '_' + blockIndex + '_enter');
+        var closeBlock = $('#' + type + '_' + blockIndex + '_close');
         if (!startBlock[0]) return;
 
         var startPoint = startBlock.offset().top;
         var closePoint = closeBlock.offset().top; 
 
         var cls = (blockIndex % 2 == 0) ? 'even' : 'odd';
-        var colorBlock = $('<div class="block block_' + cls + '" id="block_' + blockIndex + '"></div>');
+        var colorBlock = $('<div class="' + type + ' ' + type + '_' + cls + '" id="' + type + '_' + blockIndex + '"></div>');
 
         colorBlock.css({top: startPoint});
         colorBlock.width(targetWidth);
         colorBlock.height(closePoint - startPoint);
 
         target.append(colorBlock);
-        renderedColorBlocks[blockIndex] = colorBlock;
-    }
+        return colorBlock;
+    };
 
-    window.renderColorBlock = renderColorBlock;
-
-    function renderDocument(data) {
+    app.prototype.renderDocumentHTML = function(data, target) {
         var title = $('.main-title');
-        var doc = $('.wrap');
         var contentHTML = '';
         var tocHTML = '<h1>Оглавление</h1><ul class="menu none">';
 
@@ -61,19 +97,23 @@
             }
 
             var content = part.content.replace(/background: #ffffff/g, '');
-            contentHTML += ' %%' + index + ' <h1>' + part.title + '</h1>' + content + ' ' + index + '%% ';
-            tocHTML += '<li><a href="#block_' + index + '" onclick="renderColorBlock(' + index + ')">' + part.title + '</a></li>';
+
+            contentHTML += '<span id="part_' + index + '_enter" class="part_' + index + ' part_start"><a name="part_' + index + '"></a></span>'
+                + '<h1>' + part.title + '</h1>' + content
+                + '<span id="part_' + index + '_close" class="part_' + index + ' part_close"></span>';
+
+            tocHTML += '<li><a href="#part_' + index + '" onclick="app.gotoColorPart(' + index + ')">' + part.title + '</a></li>';
         });
 
-        doc[0].innerHTML = tocHTML + '</ul>' + contentHTML;
-    }
+        return target[0].innerHTML = tocHTML + '</ul>' + contentHTML;
+    };
 
-    function loadDocument() {
-        $.ajax({url: 'data/data.json', dataType: "json"}).done(function(response) {
-            renderDocument(response);
-            addColorBlocks();
+    app.prototype.loadDocumentJSON = function(target) {
+        var self = this;
+        
+        return $.ajax({url: 'data/data.json', dataType: "json"}).done(function(response) {
+            self.html = self.renderDocumentHTML(response, target);
+            self.addColorBlocks(target);
         });
-    }
-
-    $(loadDocument);
+    };
 })();
